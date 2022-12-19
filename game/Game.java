e0fbcf254d773886a17492257f5f9a82be783310
Jack Amy <jack.amy@moneyfarm.com>
2022-12-19 20:52:46 +0000

    Game class and lister progress interface

---
+++ game/Game.java
@@ -0,0 +1,19 @@
+package main.game;
+
+import main.GameObjects.Player;
+
+import javax.swing.*;
+import java.util.Scanner;
+
+/*
+ * A class to encapsulate the logic for all game states
+ * 
+ * The game starts from L1.
+ * If the player completes L1, they move to L2. 
+ * If the player fails at any point, the game ends. 
+ * The player wins by completing both levels.
+ */
+
+ public class Game implements ProgressListener {
+    
+ }
