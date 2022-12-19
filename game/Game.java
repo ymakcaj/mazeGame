bdf7f5e042766947831fc02cae7aaa1d0160c4ba
Jack Amy <jack.amy@moneyfarm.com>
2022-12-19 21:19:25 +0000

    Game should now be finished

--- game/Game.java
+++ game/Game.java
@@ -23,4 +23,98 @@ import java.util.Scanner;
     private Display display;
     private LevelOne levelOne;
     private LevelTwo levelTwo;
+
+    public Game() {
+        init();
+        setupWindowforKeyListener();
+        startLevelOne():
+    }
+
+    /*
+     * Starts the game by asking the player for the height and width of the maze.
+     * The larger the maze, the more difficult.
+     * <p>
+     * The height and width must be at least 5 for the 5 game objects in L1.
+     * Game objects will be scattered randomly, with no game objects in the same 
+     * row/column - therefore 5/5 is the minimum size 
+     */
+    private void init() {
+        Scanner s = new Scanner(System.in);
+        int mazeHeight, mazeWidth;
+        do {
+            System.out.println("Enter maze height (min 5): ");
+            while (!s.hasNextInt()) {
+                System.out.println("That's not a number! Enter again: ");
+                s.next();
+            }
+            mazeHeight = s.nextInt();
+        } while (mazeHeight < 5);
+            do {
+                System.out.println("Enter maze width (min 5): ");
+                while (!s.hasNextInt()) {
+                    System.out.println("That's not a number! Enter again: ");
+                    s.next();
+            }
+            mazeWidth = s.nextInt();
+        } while (mazeWidth < 5);
+
+        gameMap = new GameMap(mazeHeight, mazeWidth);
+        player = new Player(gameMap.getRandomPosition);
+        display = new Display(gameMap.getMap(), player);
+        display.gameIntroMessage();
+    }
+
+    /*
+     * We create a window at the top left of the screen.
+     * <p>
+     * KeyEvents need JNI or GUI to be detected.
+     * By setting up a small window and using the KeyListener in the window,
+     * the player can control the movement by using the arrow keys.
+     */
+    private void setupWindowforKeyListener() {
+        jFrame = new JFrame();
+        jFrame.setVisible(true);
+        jFrame.setSize(100,100);
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    /*
+     * Initialises L1, setting up ProgressListener and KeyListener before starting
+     */
+    private void startLevelOne() {
+        levelOne = new LevelOne(gameMap, player, display);
+        levelOne.addProgressListener(this);
+        jFrame.addKeyListener(levelOne);
+        levelOne.start();
+        }
+    /**
+     * Initialises L2, setting up ProgressListener and KeyListener before starting
+     */
+    private void startLevelTwo() {
+        levelTwo = new LevelTwo(gameMap, player, display);
+        levelTwo.setProgressListener(this);
+        jFrame.addKeyListener(levelTwo);
+        levelTwo.start();
+    }
+
+    @Override
+    public void levelOneCompleted() {
+        jFrame.removeKeyListener(levelOne);
+        display.nextLevelMessage();
+        startLevelTwo();
+    }
+
+    @Override
+    public void levelTwoCompleted() {
+        jFrame.removeKeyListener(levelTwo);
+        display.winMessage();
+        System.exit(0);
+    }
+
+    @Override
+    public void levelFailed() {
+        display.loseMessage();
+        System.exit(0);
+    }
+}
  }
