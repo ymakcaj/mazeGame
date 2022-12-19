4e2491ffa3c02385a1d7cc00bcdd029084581b69
Jack Amy <jack.amy@moneyfarm.com>
2022-12-19 21:32:05 +0000

    Starting on GameMap

---
+++ game/GameMap.java
@@ -0,0 +1,21 @@
+package main.game;
+
+import main.GameObjects.GameObject;
+import main.GameObjects.Moveable;
+import main.GameObjects.Player;
+import main.GameObjects.Position;
+import main.MazeGenerator.MazeGenerator;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+import static main.MazeGenerator.MazeGenerator.EMPTY_SPACE;
+
+/*
+ * This class encapsulates the maze with game objects and the methods to add or
+ * remove objects from the maze
+ */
+
+ public class GameMap {
+    
+ }
