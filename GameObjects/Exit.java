bcd81af75654027db9fb9d0eb6d9af29ce91590c
Jack Amy <jack.amy@moneyfarm.com>
2022-12-19 22:54:21 +0000

    finishing  GameObjects/Exit

--- GameObjects/Exit.java
+++ GameObjects/Exit.java
@@ -21,4 +21,29 @@ public class Exit extends GameObject{
      * @param player  the {@link Player} of this game
      * @return a {@link Position} in an opposite quadrant
      */
+    public static Position generateExitPosition(GameMap gameMap, Player player) {
+        int x, y;
+        if (player.getX() > gameMap.getWidth() / 2) {
+            if (player.getY() > gameMap.getHeight() / 2) {
+                // player is in quadrant 4, set exit to quadrant 2
+                x = 2;
+                y = 1;
+            } else {
+                // player is in quadrant 1, set exit to quadrant 3
+                x = 2;
+                y = gameMap.getHeight() - 2;
+            }
+        } else {
+            if (player.getY() > gameMap.getHeight() / 2) {
+                // player is in quadrant 3, set exit to quadrant 1
+                x = gameMap.getWidth() - 3;
+                y = 1;
+            } else {
+                // player is in quadrant 2, set exit to quadrant 4
+                x = gameMap.getWidth() - 3;
+                y = gameMap.getHeight() - 2;
+            }
+        }
+        return new Position(x, y);
+    }
 }
